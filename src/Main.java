733230f75a3b9233c435263217ba3a08f8512dc4
yana_klimovich <yanachka.klimovich@gmail.com>
2025-05-13 22:34:57 +0300

    task

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,17 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       int min = 0;
-       int max = 0;
-       int[]arr = {45,78,-15,12,34,6,90,7,-2,-45};
-        for (int j : arr) {
-            if (j < min) {
-                min = j;
-            }
-            if (j > max) {
-                max = j;
-            }
-        }
-System.out.println(min);
-System.out.println(max);
-
-        int[]arr1 = {45,78,-15,12,34,6,90,7,-2,-45};
-        int[]num =new int[10];
-        int[]num2 = new int[10];
-        int index1 = 0;
-        int index2 = 0;
-        for (int j : arr1) {
-            if (j % 2 != 0) {
-                num[index1] = j;
-                index1++;
-
-            } else {
-                num2[index2] = j;
-                index2++;
-
+        int[][] array = new int[10][10];
+        for (int i = 0; i < array.length; i++){
+            for(int j = 0, j < array[i].length; j ++){
+                System.out.println(Arrays.deepToString(array));
 
             }
         }
-        System.out.println("нечётные:");
-        for (int j : num) {
-            if (j == 0) {
-                continue;
 
-            }
-            System.out.println(j);
-
-        }
-        System.out.println("чётные:");
-        for (int j : num2) {
-            if (j == 0) {
-                continue;
-
-            }
-            System.out.println(j);
-        }
 
 
     }
